53fe7c65dfcde1334ff419755c29fa0b2de3dcf8
nahome03 <nahome.lezama@gmail.com>
2022-04-14 12:19:57 -0500

    Order status class outputs the status of the order with a time stamp

--- OrderStatus.cpp
+++ OrderStatus.cpp
@@ -1,1 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "OrderStatus.h"
+#include <string>
+#include<iostream>
+#include<ctime>
+
+using namespace std;
+
+//fuction receives int status
+void OrderStatus::displayOrderStatus(int status) {
+	string now;
+
+	//switch between the different types of status and prints time
+	switch (status) {
+		case 0:
+		cout << "Order is being Reviewed\n";
+		now = orderStatusTimer();
+		cout << now << endl;
+		break;
+		case 1:
+		cout << "Order has been Confirmed\n";
+		now = orderStatusTimer();
+		cout << now << endl;
+		break;
+		case 2:
+		cout << "Order is being Prepared\n"; 
+		now = orderStatusTimer();
+		cout << now << endl;
+		break;
+		case 3:
+		cout << "Order has been Completed\n"; 
+		now = orderStatusTimer();
+		cout << now << endl;
+		break;
+		case 4:
+		cout << "Order has been Delivered\n"; 
+		break;
+		default:
+		cout << "Error";
+	}
+}
+
+string OrderStatus::orderStatusTimer() {
+
+	time_t t; // t passed as argument in function time()
+	struct tm* tt; // decalring variable for localtime()
+	time(&t); //passing argument to time()
+	tt = localtime(&t);
+	string time_now = asctime(tt);	
+	return time_now;//returns variable with time
+}
+
--- OrderStatus.h
+++ OrderStatus.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <string>
+using namespace std;
+
+class OrderStatus {
+public:
+	 
+	void displayOrderStatus(int status);
+	string orderStatusTimer();
+};
