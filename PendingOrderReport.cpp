af72c8f3c4c76b25e7c4ff923df4f29e92954290
nahome03 <nahome.lezama@gmail.com>
2022-04-14 12:26:08 -0500

    Order report class prints out the information of an order

--- PendingOrderReport.cpp
+++ PendingOrderReport.cpp
@@ -1,1 +1,45 @@
 #include "PendingOrderReport.h"
+
+using namespace std;
+
+//Receives all information from 1 order
+void PendingOrderReport::Orders(int OrderID, vector<string>& ItemList, 
+	vector<int>& quantity, vector<string>& comments, string orderStatus) {
+	
+	cout << "-------------------------"<<endl;
+	cout << "Order ID: " << OrderID << endl;
+	//for loop prints items
+	for (int i = 0; i < ItemList.size(); i++){
+		cout << ItemList[i] << " ";
+		//function call to print quantity of items
+		displayQuantity(quantity, i);
+		//function call to print comments of items
+		displayComments(comments, i);
+		cout << endl;
+	}
+	
+	cout << "Order Status: " << orderStatus << endl;;
+	cout << "-------------------------";
+}
+//function with for loop to print quantity of items
+void PendingOrderReport::displayQuantity(vector<int>& quantity, int index) {
+	for (int i = index; i < quantity.size(); i++) {
+		cout << "qty(" << quantity[i] << ")" << endl;
+		break;//stops for loop to only print quantity for index
+	}
+
+}
+//function with for loop to print comments, if any, of items
+void PendingOrderReport::displayComments(vector<string>& comments, int index) {
+		for (int i = index; i < comments.size(); i++) {
+			if (comments[i] == " ") {
+				cout << "Comments: None"<<endl;
+				break;
+			}
+			else {
+				cout << "Comments: " << comments[i] << endl;
+				break;
+			}
+	}
+
+}
--- PendingOrderReport.h
+++ PendingOrderReport.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <iostream>
+#include<fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class PendingOrderReport {
+private:
+
+public:
+	static void Orders(int OrderID, vector<string>& ItemList, 
+		vector<int>& quantity, vector<string>& comments, string orderSattus);
+	static void displayQuantity(vector<int>& quantity, int index);
+	static void displayComments(vector<string>& comments, int index);
+
+};
