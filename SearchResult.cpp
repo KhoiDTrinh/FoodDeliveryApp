9e567b809333edab45a745937c7bf5bbddb40f4e
William Cao <caop5@gator.uhd.edu>
2022-04-21 19:28:33 -0500

    Search Result

--- SearchResult.cpp
+++ SearchResult.cpp
@@ -10,7 +10,7 @@ int Search_Result::display_Search_Results(vector<int> restaurantID)
 		if (display_Restaurant_Summary(restaurantID) == false) {
 			throw 0;
 		}
-		for (int x = 0; x < restaurantID.size(); x++) {
+		for (int size = 0; size < restaurantID.size(); size++) {
 			display_Restaurant_Summary(restaurantID);
 		}
 		return 1;
@@ -27,6 +27,20 @@ int Search_Result::display_Search_Results(vector<int> restaurantID)
 
 int Search_Result::display_Restaurant_Summary(vector<int> restaurantID)
 {
-	/* cout resturatn data. */
+	try
+	{
+		if (restaurantID.size() == 0) {
+			throw 0;
+		}
+		for (int size = 0; size < restaurantID.size(); size++) {
+			cout << restaurantID[size] << endl;
+		}
+
+	}
+	catch (int value)
+	{
+		return value;
+		cout << "No Search Result\n";
+	}
 	return 1;
 }
