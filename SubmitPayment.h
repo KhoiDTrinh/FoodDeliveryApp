332479514dbd15e10559b3e67d9c581c4970bb5a
Vanessacorral20 <103229971+Vanessacorral20@users.noreply.github.com>
2022-04-26 14:46:38 -0500

    Updated Submit Payment header file

--- SubmitPayment.h
+++ SubmitPayment.h
@@ -11,10 +11,10 @@ private:
 public: 
 	static const struct PaymentInformation
 	{
-		int ccNumber;//cardNumber
-		int experation;
-		int cvc;
-		string name;
+		int ccNumber;	//cardNumber
+		int experation; // Date of Experation
+		int cvc;        // CVC/ security number
+		string name;    // Name on the card
 	};
 
 	static bool submitPayment(int paymentM, PaymentInformation paymentInfo, float price)
@@ -24,14 +24,16 @@ public:
 
 		if (card_info.is_open())
 		{
-			card_info << paymentM;			//It will write into the file the type (in number) of payment: Visa = 1, Mastercard = 2, Ame. Ex = 3.
+			card_info << paymentM;			// It will write into the file the type (in number) of payment: Visa = 1, Mastercard = 2, Ame. Ex = 3.
 
-			PaymentInformation payI;
-			//card_info << payI.lat << ' ' << ;
+			PaymentInformation payI;		// Identify paymentInfo as payI to pass numbers and name on card to a file
+			card_info << payI.ccNumber;		// Push the ccNumber
+			card_info << payI.cvc;			// Push the cvc/ security number
+			card_info << payI.experation;   // Push the experation date
+			card_info << payI.name;			// Push the name on the card
+
 			card_info << price;				// It will write into the file the total price that the person will pay.
 
 		}
-
-
 	};
 };
