1a4efb8db110f3f52ba12f23542d5cefd7a1738b
Vanessacorral20 <103229971+Vanessacorral20@users.noreply.github.com>
2022-04-25 21:56:41 -0500

    Update SubmitOrder.h

--- SubmitOrder.h
+++ SubmitOrder.h
@@ -1,15 +1,1 @@
 #pragma once
-#include <iostream>
-#include <string>
-using namespace std;
-
-class PaymentProcessorClass
-{
-	static const struct PaymentInformation
-	{
-		int ccNumber;
-		int experation;
-		int cvc;
-		string name;
-	};
-};
