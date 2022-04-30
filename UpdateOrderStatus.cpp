54887c73c3d92f184cf1584be57f499022cfe580
William Cao <caop5@gator.uhd.edu>
2022-04-30 11:30:55 -0500

    Update Order Status

--- UpdateOrderStatus.cpp
+++ UpdateOrderStatus.cpp
@@ -1,5 +1,13 @@
 #include "UpdateOrderStatus.h"
 
+/*
+Date: April 29, 2022
+Name: William Cao(Phuoc), Khoi Trinh
+Artifact Name: Update Order Status Function
+
+Description:
+	Mark the order status, and send that to Order class with Order ID.
+*/
 int UpdateOrderStatus::update_order_status(int order_id, int status) {
 	Order order(order_id);
 	return order.update_order_status(status);
