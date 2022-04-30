2993c749bf221b992af1f172facd7dced02af1f3
Khoi Trinh <khoitrinh1@hotmail.com>
2022-04-30 13:22:20 -0500

    UpdateInfo, Main Comments

--- FoodDeliveryApp.cpp
+++ FoodDeliveryApp.cpp
@@ -4,6 +4,10 @@
 
 using namespace std;
 	
+//Main Driver
+//Creates UserInterface object and runs it
+//Khoi Trinh
+//04/04/22
 int main() {
 	UserInterface ui;
 	ui.run();
--- UpdateInfo.cpp
+++ UpdateInfo.cpp
@@ -2,16 +2,23 @@
 
 //-------------------- Public Functions --------------------
 
+//Update User Info
 //Takes user input from UserInterface and updates the user record in the database
 //Return values:
 //	1 = update successful;
 //	0 = unidentified error;
 //	-1 = user not found in database;
+//Khoi Trinh
+//04/14/22
 int UpdateInfo::update_user_info(vector<string>& input, UserInformation*& user_record) {
 	int result = user_record->update_user_record(input);
 	return result;
 }
 
+//Validate Login
+//Hashes provided password and checks to see if it matches database
+//Khoi Trinh
+//04/14/22
 bool UpdateInfo::validate_login(string username, string password, UserInformation*& user_record) {
 	return (!username.compare(user_record->get_username()) && !UserInformation::hash_password(password).compare(user_record->get_hashed_password()) );
 }
