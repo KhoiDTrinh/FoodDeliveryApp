fd0aa60a0c473868c0010cb89ad549159c5a10c2
nahome03 <nahome.lezama@gmail.com>
2022-04-30 12:02:06 -0500

    inline and prologue comments added

--- ShowPendingOrders.cpp
+++ ShowPendingOrders.cpp
@@ -1,6 +1,14 @@
-#include "ShowPendingOrders.h"
+#include"ShowPendingOrders.h"
+
+/*Artifact name: show_pending_orders
+* Receives the restaurant_id to print out their pending orders
+* Mitzi Lezama
+* 4/29/2022
+*/ 
 
 int ShowPendingOrders::show_pending_orders(int retaurant_id) {
+	//vector int order_ids of all current active orders
 	vector<int> order_ids = Order::get_active_orders_restaurants(retaurant_id);
+	//Pending Order Report class will print out all detail from orders
 	return PendingOrderReport::generate_pending_orders_report(order_ids);
 }
